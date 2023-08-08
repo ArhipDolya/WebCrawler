83d6c945a2bec71628b4bda8997ce6aacdca16ab
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-08-08 10:40:23 +0300

    Refactor normalizeURL function and add some tests

--- crawl.js
+++ crawl.js
@@ -3,11 +3,13 @@ const { parse } = require('url');
 function normalizeURL(URLString) {
     try {
         const urlObject = parse(URLString);
-        const normalizedURL = `${urlObject.protocol}//${urlObject.hostname}${urlObject.pathname}`;
-        return normalizedURL.toLowerCase();
+        const normalizedHostname = urlObject.hostname.toLowerCase();
+        const normalizedPathname = urlObject.pathname.replace(/\/+$/, '');
+        const normalizedURL = `${normalizedHostname}${normalizedPathname}`;
+        return normalizedURL;
     } catch (error) {
         console.error('Invalid URL:', URLString);
-        return null; 
+        return null;
     }
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -2,8 +2,29 @@ const {normalizeURL} = require('./crawl.js');
 const {test, expect} = require("@jest/globals")
 
 test("normalizeURL", () => {
-    const URL = 'HTTP://example.com/PaTh//';
+    const URL = 'https://blog.boot.dev/path';
     const output = normalizeURL(URL);
-    const expected = "http://example.com/path//"
+    const expected = "blog.boot.dev/path"
+    expect(output).toEqual(expected)
+})
+
+test("normalizeURL", () => {
+    const URL = 'https://blog.boot.dev/path/';
+    const output = normalizeURL(URL);
+    const expected = "blog.boot.dev/path"
+    expect(output).toEqual(expected)
+})
+
+test("normalizeURL Capitals", () => {
+    const URL = 'https://BLOG.boot.dev/path/';
+    const output = normalizeURL(URL);
+    const expected = "blog.boot.dev/path"
+    expect(output).toEqual(expected)
+})
+
+test("normalizeURL Capitals", () => {
+    const URL = 'http://BLOG.boot.dev/path/';
+    const output = normalizeURL(URL);
+    const expected = "blog.boot.dev/path"
     expect(output).toEqual(expected)
 })
